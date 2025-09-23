2fde113d73e59f139e19754949c9533b2827d746
Nassar <mohammednassar772003@gmail.com>
2025-09-23 16:55:11 +0300

    add igeneric repo and dbcontext plus config for models

---
+++ Ecom.infrastructure/Data/Congig/CategoryConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Ecom.Core.Entities.Product;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.infrastructure.Data.Congig;
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.Property( x => x.Name).IsRequired().HasMaxLength(30);    
+        builder.Property( x => x.Id).IsRequired();    
+    }
+}
---
+++ Ecom.infrastructure/Reposatries/GenericRepositry.cs
@@ -0,0 +1,46 @@
+﻿using Ecom.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.infrastructure.Reposatries;
+public class GenericRepositry<T> : IGenericRepositry<T> where T : class
+{
+    public Task AddAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<T>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<T>> GetAllAsync(params Expression<Func<T, object>>[] includes)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> GetByIDAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> GetByIDAsync(int id, params Expression<Func<T, object>>[] includes)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+}
