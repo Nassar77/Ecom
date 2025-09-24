f6a6469eb67215d2584b5461e7d427afb8328467
Nassar <mohammednassar772003@gmail.com>
2025-09-24 22:57:13 +0300

    first controller

---
+++ Ecom.Api/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Ecom.Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecom_Api.Controllers;
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class BaseController(IUnitOfWork work) : ControllerBase
+{
+    protected readonly IUnitOfWork _work = work;
+}
---
+++ Ecom.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using Ecom.Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecom_Api.Controllers;
+
+public class CategoriesController(IUnitOfWork work) : BaseController(work)
+{
+    [HttpGet("")]
+    public async Task<IActionResult> GetAll()
+    {
+        try
+        {
+            var categories=await _work.CategoryRepositry.GetAllAsync();
+            if (categories is null)
+                return BadRequest();
+            return Ok(categories);
+
+        }
+        catch (Exception ex) 
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+}
