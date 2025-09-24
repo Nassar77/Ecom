40298e1aef198f2b295b417b738047515825c860
Nassar <mohammednassar772003@gmail.com>
2025-09-24 21:36:03 +0300

    add connection string

--- Ecom.infrastructure/infrastructureRegisteration.cs
+++ Ecom.infrastructure/infrastructureRegisteration.cs
@@ -1,12 +1,15 @@
 ﻿using Ecom.Core.Interfaces;
+using Ecom.infrastructure.Data;
 using Ecom.infrastructure.Reposatries;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 
 namespace Ecom.infrastructure;
 public static class infrastructureRegisteration
 {
-    public static IServiceCollection infrastructureConfiguration(this IServiceCollection services)
+    public static IServiceCollection infrastructureConfiguration(this IServiceCollection services,IConfiguration configuration)
     {
         services.AddScoped(typeof(IGenericRepositry<>), typeof(GenericRepositry<>));
 
@@ -15,6 +18,10 @@ public static class infrastructureRegisteration
         //services.AddScoped<IProductRepositry, ProductRepositry>();
 
         services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("EcomDatabase"));
+        });
         return services;
     }
 }
